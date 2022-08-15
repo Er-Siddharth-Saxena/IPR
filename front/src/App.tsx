0c3ab5ed29338fa577bf23c214626eef79d8251c
Priyanshu Upadhyay <puofficialid@gmail.com>
2022-08-15 18:59:40 +0530

    Added temporary footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,19 @@
 import classNames from 'classnames';
+import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar';
 import { RouteList } from './components/routes/routeList';
 
 function App() {
   return (
+    <div>
     <Navbar>
       <div className={classNames({
-        "px-12 pt-8": true,
+        "px-12 py-4": true,
       })}><RouteList/></div>
     </Navbar>
+    jkdfsakj
+    <Footer/>
+    </div>
   );
 }
 
