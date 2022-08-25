c152831545b0a186ade1d7eed7ad182fb02127ec
Priyanshu Upadhyay <puofficialid@gmail.com>
2022-08-25 17:35:49 +0530

    Added form for ipr

--- front/src/pages/registerForPatent/registerForPatent.tsx
+++ front/src/pages/registerForPatent/registerForPatent.tsx
@@ -5,8 +5,8 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import AddIcon from '@mui/icons-material/Add';
-import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Register = () => {
   const user = useLoginStore((state) => state.user);
@@ -39,21 +39,24 @@ const Register = () => {
     setInventors(newField);
   };
 
-  const handleChange = (i: number, e:any) => {
+  const handleChange = (i: number, e: any) => {
     let newField: any = [...inventors];
     newField[i][e.target.name] = e.target.value;
     setInventors(newField);
   };
 
   return (
-    <div className="min-h-full flex items-center justify-center">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-full flex  items-center justify-center">
+      <div className="max-w-2xl w-full space-y-8 border-[1px] p-2 rounded-lg">
         <div>
           <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">
             IPR Application Form
           </h2>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form
+          onSubmit={handleSubmit}
+          className="lg:grid grid-flow-row grid-cols-2 gap-4"
+        >
           <div>
             <label htmlFor="name">Name</label>
             <input
@@ -126,18 +129,18 @@ const Register = () => {
           </div>
           <div>
             <label htmlFor="address">Address</label>
-            <textarea
+            <input
               id="address"
               name="address"
               autoComplete="address"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="000-000-0000"
+              placeholder="Address"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
             />
           </div>
-          <div>
+          <div className="col-span-2">
             <label htmlFor="invention">Title of the invention</label>
             <input
               id="invention"
@@ -151,72 +154,102 @@ const Register = () => {
               onChange={(e) => setInvention(e.target.value)}
             />
           </div>
-          <div className = "flex flex-row justify-between items-center mt-4">
+          <div className="flex flex-row justify-between items-center mt-4 col-span-2">
             <div>Inventors</div>
-            <div onClick={addInventors} className="cursor-pointer">Add an inventor<AddIcon className="text-green-500"/></div>
+            <div onClick={addInventors} className="cursor-pointer">
+              Add an inventor
+              <AddIcon className="text-green-500" />
+            </div>
           </div>
           {inventors.map((element, index) => {
             return (
-              <div className="p-2 border-[0.5]" key = {index}>
-                <div className="flex flex-row justify-between"><label>{`Inventor ${index+1} Name`}</label>
-                <div>
-                {index ? (
-                  <button
-                    type="button"
-                    className="flex flex-row cursor-pointer"
-                    onClick={() => removeInventors(index)}
-                  >
-                    Remove
-                    <CloseIcon className= "text-red-600"/>
-                  </button>
-                ) : null}
-                </div>
+              <div
+                className="p-2 my-2 border-2 border-green-300 rounded-md"
+                key={index}
+              >
+                <div className="flex flex-row justify-between">
+                  <label>{`Inventor ${index + 1}`}</label>
+                  <div>
+                    {index ? (
+                      <button
+                        type="button"
+                        className="flex flex-row cursor-pointer"
+                        onClick={() => removeInventors(index)}
+                      >
+                        Remove
+                        <CloseIcon className="text-red-600" />
+                      </button>
+                    ) : null}
+                  </div>
                 </div>
                 <input
                   type="text"
-                  name={`Inventor ${index+1} Name`}
+                  name={`Inventor ${index + 1} Name`}
                   value={element.name}
                   onChange={(e) => handleChange(index, e)}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="mt-1 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Inventor Name"
                 />
-                <label>{`Designation ${index+1} Name`}</label>
                 <input
                   type="text"
-                  name={`Designation ${index+1} Name`}
+                  name={`Designation ${index + 1} Name`}
                   value={element.email}
                   onChange={(e) => handleChange(index, e)}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="mt-1 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Designation"
                 />
-                <label>{`Address ${index+1} Name`}</label>
                 <input
                   type="text"
-                  name={`Address ${index+1} Name`}
+                  name={`Address ${index + 1} Name`}
                   value={element.email}
                   onChange={(e) => handleChange(index, e)}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="mt-1 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Address"
                 />
-                <label>{`Phone ${index+1} Name`}</label>
                 <input
                   type="text"
-                  name={`Phone ${index+1} Name`}
-                  value={element.email}
+                  name={`Phone ${index + 1} Name`}
+                  value={element.phone}
                   onChange={(e) => handleChange(index, e)}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="mt-1 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Phone"
                 />
-                <label>{`Email ${index+1} Name`}</label>
                 <input
                   type="text"
-                  name={`Email ${index+1} Name`}
+                  name={`Email ${index + 1} Name`}
                   value={element.email}
                   onChange={(e) => handleChange(index, e)}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="mt-1 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email"
                 />
-                
               </div>
             );
           })}
+          {/* (a) Description of Invention:
+ (b) Novel Features:
+ (c) Whether the invention relates to a process/product/both?
+ (d)What are the advantages of the present invention over the comparable inventions?
+ (e) Has the invention been tested experimentally? Are experimental data available? */}
 
-          <div>
+          <div className="flex flex-row justify-between items-center mt-4 col-span-2">
+            <div>Details regarding invention</div>
+          </div>
+          <div className="col-span-2">
+            <label htmlFor="invention">Brief Description</label>
+            <input
+              id="invention"
+              name="invention"
+              type="text"
+              autoComplete="invention"
+              required
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Title of the invention"
+              value={invention}
+              onChange={(e) => setInvention(e.target.value)}
+            />
+          </div>
+
+          <div className="col-span-2">
             <button
               type="submit"
               className="group mt-4 relative w-full flex justify-center py-2 px-4 border-transparent text-s font-medium rounded-md text-white border-[#B8B8B8] border-[1px] bg-[#00D100] hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
