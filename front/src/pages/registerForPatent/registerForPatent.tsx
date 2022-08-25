049a953425c4e97b36022fd578a2cacb039267a5
Priyanshu Upadhyay <puofficialid@gmail.com>
2022-08-25 16:32:17 +0530

    added dynamic form field creator

--- front/src/pages/registerForPatent/registerForPatent.tsx
+++ front/src/pages/registerForPatent/registerForPatent.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useLoginStore } from "../../stores/stores";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
 
-
-
-const Register = ()=>{
+const Register = () => {
   const user = useLoginStore((state) => state.user);
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -22,14 +22,35 @@ const Register = ()=>{
   const [dob, setDob] = React.useState("");
   const [gender, setGender] = React.useState("");
   const [invention, setInvention] = React.useState("");
-  const [noOfInventors, setNoOfInventors] = React.useState(1);
+  const [inventors, setInventors] = React.useState([
+    { name: "", designation: "", address: "", phone: "", email: "" },
+  ]);
+
+  const addInventors = () => {
+    setInventors([
+      ...inventors,
+      { name: "", designation: "", address: "", phone: "", email: "" },
+    ]);
+  };
+
+  const removeInventors = (i: number) => {
+    let newField: any = [...inventors];
+    newField.splice(i, 1);
+    setInventors(newField);
+  };
+
+  const handleChange = (i: number, e:any) => {
+    let newField: any = [...inventors];
+    newField[i][e.target.name] = e.target.value;
+    setInventors(newField);
+  };
 
   return (
     <div className="min-h-full flex items-center justify-center">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">
-            Register for Patent
+            IPR Application Form
           </h2>
         </div>
         <form onSubmit={handleSubmit}>
@@ -119,7 +140,6 @@ const Register = ()=>{
           <div>
             <label htmlFor="invention">Title of the invention</label>
             <input
-
               id="invention"
               name="invention"
               type="text"
@@ -131,25 +151,71 @@ const Register = ()=>{
               onChange={(e) => setInvention(e.target.value)}
             />
           </div>
-          <div>
-            <label htmlFor="noOfInventors">Number of inventors</label>
-            <input
-
-              id="noOfInventors"
-              name="noOfInventors"
-              type="number"
-              autoComplete="noOfInventors"
-              required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Number of inventors"
-              value={noOfInventors}
-              onChange={(e) => setNoOfInventors(parseInt(e.target.value))}
-              max={5}
-              min={1}
-              />
+          <div className = "flex flex-row justify-between items-center mt-4">
+            <div>Inventors</div>
+            <div onClick={addInventors} className="cursor-pointer">Add an inventor<AddIcon className="text-green-500"/></div>
           </div>
-          <div id="inventors"></div>
-          
+          {inventors.map((element, index) => {
+            return (
+              <div className="p-2 border-[0.5]" key = {index}>
+                <div className="flex flex-row justify-between"><label>{`Inventor ${index+1} Name`}</label>
+                <div>
+                {index ? (
+                  <button
+                    type="button"
+                    className="flex flex-row cursor-pointer"
+                    onClick={() => removeInventors(index)}
+                  >
+                    Remove
+                    <CloseIcon className= "text-red-600"/>
+                  </button>
+                ) : null}
+                </div>
+                </div>
+                <input
+                  type="text"
+                  name={`Inventor ${index+1} Name`}
+                  value={element.name}
+                  onChange={(e) => handleChange(index, e)}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                <label>{`Designation ${index+1} Name`}</label>
+                <input
+                  type="text"
+                  name={`Designation ${index+1} Name`}
+                  value={element.email}
+                  onChange={(e) => handleChange(index, e)}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                <label>{`Address ${index+1} Name`}</label>
+                <input
+                  type="text"
+                  name={`Address ${index+1} Name`}
+                  value={element.email}
+                  onChange={(e) => handleChange(index, e)}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                <label>{`Phone ${index+1} Name`}</label>
+                <input
+                  type="text"
+                  name={`Phone ${index+1} Name`}
+                  value={element.email}
+                  onChange={(e) => handleChange(index, e)}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                <label>{`Email ${index+1} Name`}</label>
+                <input
+                  type="text"
+                  name={`Email ${index+1} Name`}
+                  value={element.email}
+                  onChange={(e) => handleChange(index, e)}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                
+              </div>
+            );
+          })}
+
           <div>
             <button
               type="submit"
@@ -161,11 +227,11 @@ const Register = ()=>{
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const RegisterForPatent = () => {
-  const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -178,7 +244,9 @@ const RegisterForPatent = () => {
           <Tab label="Check Staus of the application" value="2" />
         </TabList>
       </Box>
-      <TabPanel value="1"><Register/></TabPanel>
+      <TabPanel value="1">
+        <Register />
+      </TabPanel>
       <TabPanel value="2">Item Two</TabPanel>
     </TabContext>
   );
